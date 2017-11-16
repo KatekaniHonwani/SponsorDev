4368faef772d29d43cffd13c99034a53055123bb
Thulibathi <nokuthulamargaret@gmail.com>
2017-11-16 17:05:09 +0200

    Merge branch 'master' of https://github.com/The404Brainex/SponsorDev
    # Conflicts:
    #	app/src/main/java/com/katekani/laptopsponsorapp/ClientAndSponsorActivity.java
    #	app/src/main/java/com/katekani/laptopsponsorapp/UserProfileActivity.java

--- app/src/main/java/com/katekani/laptopsponsorapp/ClientAndSponsorActivity.java
+++ app/src/main/java/com/katekani/laptopsponsorapp/ClientAndSponsorActivity.java
@@ -1,34 +1,36 @@
 package com.katekani.laptopsponsorapp;
 
-import android.util.Log;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import android.widget.Toast;
-import android.view.MenuItem;
-import android.content.Intent;
-import android.content.Context;
-import android.widget.TextView;
-import android.support.v7.widget.Toolbar;
-import android.support.annotation.NonNull;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.app.AppCompatActivity;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import android.support.design.widget.NavigationView;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
 import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
 public class ClientAndSponsorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
